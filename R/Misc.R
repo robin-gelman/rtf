d5a7e81be24a1062d6f868491609339c8fab26b7
robin-gelman <47285619+robin-gelman@users.noreply.github.com>
2019-07-04 13:58:53 +0800

    Update Misc.R

--- R/Misc.R
+++ R/Misc.R
@@ -1,18 +1,18 @@
 
-addh <- function(para,rtf=rtf){
+addh <- function(para,rtf){
 	addParagraph(rtf,paste0("",para))
 	}
 	
-addp <- function(para,rtf=rtf){
+addp <- function(para,rtf){
 	addParagraph(rtf,paste0("\t",para))
 	}
 
-addc <- function(para,rtf=rtf){
+addc <- function(para,rtf){
 	addParagraph(rtf,paste0("\\qc ",para))
 	}
 
 
-cap_png <- function(pngfile,rtf=rtf) {
+cap_png <- function(pngfile,rtf) {
 	#addParagraph(rtf,deparse(substitute(pngfile)))	
 	addPng(rtf, pngfile,width=6,height=4.5)
 }
