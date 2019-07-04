ee14faa8799de981acc94b16b74e78eeeb628bde
robin-gelman <47285619+robin-gelman@users.noreply.github.com>
2019-07-04 14:03:26 +0800

    Update Misc.R

--- R/Misc.R
+++ R/Misc.R
@@ -1,18 +1,18 @@
 
-addh <- function(para,rtf){
-	addParagraph(rtf,paste0("",para))
+addh <- function(para,rtfile=rtf){
+	addParagraph(rtfile,paste0("",para))
 	}
 	
-addp <- function(para,rtf){
-	addParagraph(rtf,paste0("\t",para))
+addp <- function(para,rtfile=rtf){
+	addParagraph(rtfile,paste0("\t",para))
 	}
 
-addc <- function(para,rtf){
-	addParagraph(rtf,paste0("\\qc ",para))
+addc <- function(para,rtfile=rtf){
+	addParagraph(rtfile,paste0("\\qc ",para))
 	}
 
 
-cap_png <- function(pngfile,rtf) {
+cap_png <- function(pngfile,rtfile=rtf) {
 	#addParagraph(rtf,deparse(substitute(pngfile)))	
-	addPng(rtf, pngfile,width=6,height=4.5)
+	addPng(rtfile, pngfile,width=6,height=4.5)
 }
